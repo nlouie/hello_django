f8ef5eea651205eb9b4db2dfb70580cd27b65af0
nlouie <nlouie@bu.edu>
2017-08-09 22:10:07 -0400

    context

--- main/views.py
+++ main/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 # django views are synonymous to a controller in the MVC framework
 
 def home(request):
-    return render(request, "main/home.html")
+    return render(request, "main/home.html", {'message': 'hi whats up'})
